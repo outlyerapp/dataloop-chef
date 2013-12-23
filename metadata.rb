cffde45392230cc3a22d6ab6238d2718dd5e2922
sacreman <sacreman@promiscuouss-MacBook-Air.local>
2013-12-23 15:07:39 +0000

    add dataloop agent de-registration on stop

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'support@dataloop.io'
 license          'All rights reserved'
 description      'Installs/Configures dataloop'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.3'
