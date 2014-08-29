2549bd9f0304aec9bdf70aaffc78005dff58f762
Matthew Macdonald-Wallace <theprofessor@threedrunkensysadsonthe.net>
2014-08-29 07:32:36 +0100

    Secure the API and fix up permissions
    The API key was previously set as an attribute meaning that anyone
    with access to the cookbook repo could see and use the API key.  This
    change moves the API Key into an Encrypted Data Bag.
    
    The permissions on /opt/dataloop were constantly being reset to
    "root:root" preventing the agent from starting properly.  This change
    creates the "dataloop" user, sets the home directory to
    "/opt/dataloop" and runs a small bash script over that directory to
    ensure that the permissions are correct.

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,6 +17,14 @@
 # limitations under the License.
 #
 
+## Setup the user account that we will run as
+user node['dataloop']['node']['user'] do
+    action :create
+    supports :mange_home => true
+    comment "Dataloop User Account"
+    home node['dataloop']['agent']['run_dir']
+end
+
 remote_file node['dataloop']['agent']['bin_dir'] do
   source node['dataloop']['agent']['uri']
   checksum node['dataloop']['agent']['checksum']
@@ -26,33 +34,59 @@ end
 directory node['dataloop']['agent']['run_dir'] do
   action :create 
   owner node['dataloop']['node']['user']
-  group "root"
+  group node['dataloop']['node']['user']
   mode 0755
   recursive true
 end
 
-execute "user owns run dir" do
-  command "chown -R node['dataloop']['node']['user'] node['dataloop']['agent']['run_dir']"
+directory "#{node['dataloop']['agent']['run_dir']}" do
+  action :create 
+  owner node['dataloop']['node']['user']
+  group node['dataloop']['node']['user']
+  mode 0755
+  recursive true
 end
+
 
 directory node['dataloop']['agent']['log_dir'] do
   action :create 
   owner node['dataloop']['node']['user']
-  group "root"
+  group node['dataloop']['node']['user']
   mode 0755
 end
 
 directory node['dataloop']['agent']['conf_dir'] do
   action :create 
   owner node['dataloop']['node']['user']
-  group "root"
+  group node['dataloop']['node']['user']
   mode 0755
 end
 
+dataloop_secret = Chef::EncryptedDataBagItem.load_secret("#{node[:dataloop][:node][:secret_key_file]}")
+dataloop_keys = Chef::EncryptedDataBagItem.load("dataloop", "keys", dataloop_secret)
+
 template node['dataloop']['agent']['init'] do
+  source "dataloop-agent.erb"
   owner "root"
   group "root"
   mode 0755
+  variables({
+      :api_key => dataloop_keys['api']
+  })
+end
+
+## I'm not particularly proud of this but for some reason the
+#permissions on /opt/dataloop keep being reset to "root:root",
+#preventing the agent from starting properly.
+#
+#This bash script forces the permissions to the correct settings and
+#allows the agent to start properly.
+bash "hack permissions" do
+    user "root"
+    cwd  "/opt"
+    code <<-EOH
+    chown -Rvf dataloop:root /opt/dataloop
+    EOH
 end
 
 service "dataloop-agent" do
