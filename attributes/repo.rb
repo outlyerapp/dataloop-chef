77532c634bd46e834ae97b3c9fbc5790bb05615f
Tom Ashley <tom.ashley@dataloop.io>
2014-11-25 14:58:23 +0000

    use stable repo paths by default

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -1,6 +1,6 @@
 case node['platform_family']
 when 'rhel', 'fedora'
-  default['dataloop']['package_repository'] = "https://download.dataloop.io/packages/unstable/rpm/centos/#{node['platform_version'].to_i}/$basearch/"
+  default['dataloop']['package_repository'] = "https://download.dataloop.io/packages/stable/rpm/$basearch/"
 when 'debian'
   default['dataloop']['package_repository'] = "https://download.dataloop.io/packages/unstable/ubuntu/"
 end
