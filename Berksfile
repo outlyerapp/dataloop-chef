b7fc65723989efed20ef6defa5d4689d30fd321b
Tom Ashley <tom.ashley@dataloop.io>
2014-11-25 16:01:38 +0000

    version contstraints

--- Berksfile
+++ Berksfile
@@ -3,4 +3,3 @@ source "https://api.berkshelf.com"
 metadata
 cookbook 'apt', '~> 2.6.0'
 cookbook 'yum', '~> 3.4.1'
-
--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures dataloop'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.6'
 
-required 'apt',             '~> 2.2'
-required 'yum',             '~> 3.4.1'
+depends 'apt',      '~> 2.6.0'
+depends 'yum',     '~> 3.4.1'
