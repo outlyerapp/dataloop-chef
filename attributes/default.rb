6ccc9e2c60afab3858b7b495a63ddf8c7ec8a9aa
Tom Ashley <tom.ashley@dataloop.io>
2014-12-19 17:54:02 +0000

    parameterize the server endpoint

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,7 @@ default['dataloop']['agent']['log_dir'] = "/var/log/dataloop"
 default['dataloop']['agent']['conf_dir'] = "/etc/dataloop"
 default['dataloop']['agent']['conf_file'] = "agent.conf"
 default['dataloop']['agent']['api_key'] = nil
+default['dataloop']['agent']['server'] = 'https://www.dataloop.io'
 
 default['dataloop']['node']['tags'] = "all"
 default['dataloop']['node']['user'] = "dataloop"
