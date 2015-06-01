c7c774322c37f4c921c5016c3cfa26f4cf1f0bc7
Tom Ashley <tom.ashley@dataloop.io>
2015-06-01 15:10:52 +0100

    share package distribution across deb and rpm
    allows kitchen test to run across all variants at once

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -1,8 +1,8 @@
 default['dataloop']['package_gpg_key'] = 'https://download.dataloop.io/pubkey.gpg'
+default['dataloop']['package_distribution'] = 'stable'
 case node['platform_family']
 when 'rhel', 'fedora'
-  default['dataloop']['package_repository'] = "https://download.dataloop.io/packages/stable/rpm/$basearch/"
+  default['dataloop']['package_repository'] = "https://download.dataloop.io/packages/#{node['dataloop']['package_distribution']}/rpm/$basearch/"
 when 'debian'
   default['dataloop']['package_repository'] = "https://download.dataloop.io/deb"
-  default['dataloop']['package_distribution'] = 'stable'
 end
