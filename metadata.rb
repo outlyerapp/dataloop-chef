cd4cfea9da9bc2ba25209b6c1ec44a0dee15be3d
Tom Ashley <tom.ashley@dataloop.io>
2015-06-01 15:11:39 +0100

    v1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@dataloop.io'
 license          'All rights reserved'
 description      'Installs/Configures dataloop'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 depends 'apt',      '~> 2.6.0'
 depends 'yum',     '~> 3.4.1'
