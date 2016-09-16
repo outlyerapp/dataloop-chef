f89af46ca8f2f145b37f3ca59a4d902f31c1d29a
Tom Ashley <tom.ashley@dataloop.io>
2016-09-16 09:39:50 +0100

    recipe to install and configure on windows

---
+++ recipes/windows.rb
@@ -0,0 +1,21 @@
+windows_package 'dataloop-agent' do
+  source 'https://download.dataloop.io/windows/latest/dataloop-agent-1.1.32-1_x86.exe'
+  remote_file_attributes ({
+    :checksum => '2cd19b931355e23eee095ffd4d8140cfe3625741eff503615c821907a3dcd546',
+    :path => 'C:\\dataloop-agent.exe'
+  })
+  #installer_type :custom
+  options ''
+end
+
+template node['dataloop']['agent']['conf_file'] do
+  # path "#{node['dataloop']['agent']['conf_dir']}/#{node['dataloop']['agent']['conf_file']}"
+  path 'c:\dataloop\agent.yaml'
+  source "agent.yaml.erb"
+  notifies :restart, "windows_service[dataloop-agent]", :delayed
+end
+
+windows_service 'dataloop-agent' do
+  action :configure_startup
+  startup_type :manual
+end
