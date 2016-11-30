79f83290ff6b5fa62c17d9d1c1ab95bf2221cb3e
Tom Ashley <tom.ashley@dataloop.io>
2016-11-30 14:15:59 +0000

    pick up rpm releasever in package repo

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -2,7 +2,7 @@ default['dataloop']['package_gpg_key'] = 'https://download.dataloop.io/pubkey.gp
 default['dataloop']['package_distribution'] = 'stable'
 case node['platform_family']
 when 'rhel', 'fedora'
-  default['dataloop']['package_repository'] = "https://download.dataloop.io/packages/#{node['dataloop']['package_distribution']}/rpm/$basearch/"
+  default['dataloop']['package_repository'] = "https://download.dataloop.io/packages/#{node['dataloop']['package_distribution']}/el$releasever/$basearch/"
 when 'debian'
   default['dataloop']['package_repository'] = "https://download.dataloop.io/deb"
 end
