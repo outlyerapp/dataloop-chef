695eaefdc961ee972825a881e875e62aaf82f6c9
Tom Ashley <tom.ashley@dataloop.io>
2017-07-18 17:26:36 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@outlyer.com'
 license          'All rights reserved'
 description      'Installs/Configures dataloop/outlyer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.1.1'
 
 depends 'apt',      '~> 2.9.2'
 depends 'yum',     '~> 3.8.2'
