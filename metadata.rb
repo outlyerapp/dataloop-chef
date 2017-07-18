56f910f2933eac81a1b67603a5458fe645d65da6
Tom Ashley <tom.ashley@dataloop.io>
2017-07-18 16:43:56 +0100

    upgrade version for statsd

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             "dataloop-agent"
 maintainer       'Dataloop Software Limited'
-maintainer_email 'support@dataloop.io'
+maintainer_email 'support@outlyer.com'
 license          'All rights reserved'
-description      'Installs/Configures dataloop'
+description      'Installs/Configures dataloop/outlyer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.6'
+version          '1.1.0'
 
 depends 'apt',      '~> 2.9.2'
 depends 'yum',     '~> 3.8.2'
